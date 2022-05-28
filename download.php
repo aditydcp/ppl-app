b5e0d5309cd74361396d023188fdc1d6d4444e2a
Aditya Cahyo <aditydcp@gmail.com>
2022-05-28 14:59:07 +0700

    implement download function

---
+++ download.php
@@ -0,0 +1,93 @@
+<?php
+
+function get_mime_type($filepath) {
+    
+    $idx = explode( '.', $filepath );
+    $count_explode = count($idx);
+    $idx = strtolower($idx[$count_explode-1]);
+
+    $mimet = array( 
+        'txt' => 'text/plain',
+        'htm' => 'text/html',
+        'html' => 'text/html',
+        'php' => 'text/html',
+        'css' => 'text/css',
+        'js' => 'application/javascript',
+        'json' => 'application/json',
+        'xml' => 'application/xml',
+        'swf' => 'application/x-shockwave-flash',
+        'flv' => 'video/x-flv',
+
+        // images
+        'png' => 'image/png',
+        'jpe' => 'image/jpeg',
+        'jpeg' => 'image/jpeg',
+        'jpg' => 'image/jpeg',
+        'gif' => 'image/gif',
+        'bmp' => 'image/bmp',
+        'ico' => 'image/vnd.microsoft.icon',
+        'tiff' => 'image/tiff',
+        'tif' => 'image/tiff',
+        'svg' => 'image/svg+xml',
+        'svgz' => 'image/svg+xml',
+
+        // archives
+        'zip' => 'application/zip',
+        'rar' => 'application/x-rar-compressed',
+        'exe' => 'application/x-msdownload',
+        'msi' => 'application/x-msdownload',
+        'cab' => 'application/vnd.ms-cab-compressed',
+
+        // audio/video
+        'mp3' => 'audio/mpeg',
+        'qt' => 'video/quicktime',
+        'mov' => 'video/quicktime',
+
+        // adobe
+        'pdf' => 'application/pdf',
+        'psd' => 'image/vnd.adobe.photoshop',
+        'ai' => 'application/postscript',
+        'eps' => 'application/postscript',
+        'ps' => 'application/postscript',
+
+        // ms office
+        'doc' => 'application/msword',
+        'rtf' => 'application/rtf',
+        'xls' => 'application/vnd.ms-excel',
+        'ppt' => 'application/vnd.ms-powerpoint',
+        'docx' => 'application/msword',
+        'xlsx' => 'application/vnd.ms-excel',
+        'pptx' => 'application/vnd.ms-powerpoint',
+
+
+        // open office
+        'odt' => 'application/vnd.oasis.opendocument.text',
+        'ods' => 'application/vnd.oasis.opendocument.spreadsheet',
+    );
+
+    if (isset( $mimet[$idx] )) {
+     return $mimet[$idx];
+    } else {
+     return 'application/octet-stream';
+    }
+}
+
+if(isset($_POST['file_download'])) {
+    // retrieve file path
+    $file_path = $_POST['file_download'];
+    // retrieve file metadata
+    $file_name = basename($file_path);
+    $file_size = filesize($file_path);
+    $file_mime = get_mime_type($file_path);
+
+    // download the copy
+    header('Content-Type: ' . $file_mime);
+    header('Content-disposition: attachment; filename=' . $file_path);
+    header('Content-Length: ' . $file_size);
+    echo(file_get_contents($file_path));
+    
+    // retrieve destination
+    $dest = $_POST['fromandto'];
+    header("Location: /ppl-app/admin.php");
+}
+?>
