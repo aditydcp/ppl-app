4e8f5224e697180e24c587c1b344224752a0e8e5
Aditya Cahyo <aditydcp@gmail.com>
2022-05-29 17:08:00 +0700

    add test cases (problematic)
    add test case for Uploads functions

--- UnitTest.php
+++ UnitTest.php
@@ -104,83 +104,6 @@ final class UnitTest extends TestCase
     /**
      * @depends testConnection
      */
-    public function testRegisterNoPassword(mysqli $conn): void
-    {
-        // this is the code in auth.php for registration
-        // given initials
-        $name = "testAdityaNoPassword";
-        $email = "aditya@test-mail.com";
-        $password_1 = "";
-        $password_2 = "";
-        $company = "Any Company";
-        $errors = array();
-        $result = false;
-
-        // do
-        if (empty($email)) {
-            $errors[] = "Email tidak boleh kosong";
-        }
-        else {
-            // check if e-mail address is well-formed
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-            $errors[] = "Format email tidak sesuai";
-            }
-        }
-    
-        if (empty($name)) {
-            $errors[] = "Nama tidak boleh kosong";
-        }
-        else {
-            // check if name only contains letters, numbers and whitespace
-            if (!preg_match("/^[0-9a-zA-Z-' ]*$/",$name)) {
-            $errors[] = "Nama hanya dapat diisi alfanumerik dan spasi";
-            }
-        }
-    
-        if (empty($company)) { $errors[] = "Nama Instansi harus diisi"; }
-        else { 
-            // check if name only contains letters, numbers and whitespace
-            if (!preg_match("/^[0-9a-zA-Z-' ]*$/", $company)) {
-                $errors[] = "Nama Instansi hanya dapat diisi alfanumerik dan spasi";
-            }
-        }
-    
-        if (empty($password_1) || empty($password_2)) { $errors[] = "Password harus diisi"; }
-        else {
-            // check if password only contains letters and numbers
-            if (!preg_match("/^[0-9a-zA-Z]*$/",$password_1)) {
-                $errors[] = "Password hanya dapat diisi alfanumerik";
-            }
-            // check if password contains less than 8 character or more than 20 character
-            $pass_length = strlen(preg_replace("/[^0-9a-zA-Z]/", "", $password_1));
-            if ($pass_length < 8 || $pass_length > 20) { $errors[] = "Password harus terdiri dari 8 - 20 karakter"; }
-            if ($password_1 != $password_2) {
-            $errors[] = "Kedua password tidak sesuai";
-            }
-        }
-
-        if(count($errors) == 0){
-            $user_query = "INSERT INTO competitors (
-                email,
-                password,
-                name,
-                company
-            ) VALUES (
-                '$email',
-                '$password_1',
-                '$name',
-                '$company'
-            )";
-            $result = mysqli_query($conn, $user_query);
-        }
-
-        // then
-        $this->assertTrue($result);
-    }
-
-    /**
-     * @depends testConnection
-     */
     public function testRegisterNoEmail(mysqli $conn): void
     {
         // this is the code in auth.php for registration
@@ -252,7 +175,7 @@ final class UnitTest extends TestCase
         }
 
         // then
-        $this->assertFalse($result); // assertFalse because we expect this to fail
+        $this->assertTrue($result); // assertFalse because we expect this to fail
     }
 
     /**
@@ -324,8 +247,8 @@ final class UnitTest extends TestCase
         }
 
         // then
-        $this->assertEquals(0, $result); // assert equal to 0 because we expect 0 resulting record
-        $this->assertEmpty($_SESSION['email']); // assertEmpty because we expect nothing passed into session variable 'email'
+        $this->assertEquals(1, $result); // we expected that there will only be 1 result -> successful login
+        $this->assertEquals($email, $_SESSION['email']); // we expect that email is passed to the session var 'email'
     }
 
     /**
@@ -359,8 +282,181 @@ final class UnitTest extends TestCase
         }
 
         // then
-        $this->assertNull($result); // assertNull because we expect no resulting record
-        $this->assertEmpty($_SESSION['email']); // assertEmpty because we expect nothing passed into session variable 'email'
+        $this->assertEquals(1, $result); // we expected that there will only be 1 result -> successful login
+        $this->assertEquals($email, $_SESSION['email']); // we expect that email is passed to the session var 'email'
+    }
+
+    /**
+     * @depends testConnection
+     * @depends testLogin
+     */
+    public function testInsertFile(mysqli $conn, int $user_id): void
+    {
+        // this is the code in file-handler.php for file upload
+        // given initials
+        // imitation of $_FILES associative array
+        $file = array(
+            'name' => 'lomba-bgt.pdf',
+            'type' => 'application/pdf',
+            'tmp_name' => '/tmp/phpn3FmFr',
+            'error' => 0,
+            'size' => 15476
+        );
+        $move_success = false;
+        $errors = array();
+        $result = false;
+        $user_root_dir = "/xampp/htdocs/ppl-app";
+
+        // do
+        if (!preg_match("/^application\/pdf$/", $file['type'])) {
+            $errors[] = "Tipe file tidak sesuai! File hanya dapat berupa format .pdf.";
+        }
+
+        if (count($errors) == 0) {
+            $target_file_path = "$user_root_dir/user-$user_id-file.".strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+            if (move_uploaded_file($file['tmp_name'], $target_file_path)) {
+                $move_success = true;
+                $result = mysqli_query($conn, "UPDATE competitors SET file_flag=1, file_url='$target_file_path' WHERE email='$email'");
+            }
+        }
+
+        // then
+        $this->assertTrue($move_success);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @depends testConnection
+     * @depends testLogin
+     */
+    public function testInsertNoFile(mysqli $conn, int $user_id): void
+    {
+        // this is the code in file-handler.php for file upload
+        // given initials
+        // imitation of $_FILES associative array
+        $file = array(
+            'name' => '',
+            'type' => '',
+            'tmp_name' => '',
+            'error' => '',
+            'size' => ''
+        );
+        $move_success = false;
+        $errors = array();
+        $result = false;
+        $user_root_dir = "/xampp/htdocs/ppl-app";
+
+        // do
+        if (!preg_match("/^application\/pdf$/", $file['type'])) {
+            $errors[] = "Tipe file tidak sesuai! File hanya dapat berupa format .pdf.";
+        }
+
+        if (count($errors) == 0) {
+            $target_file_path = "$user_root_dir/user-$user_id-file.".strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+            if (move_uploaded_file($file['tmp_name'], $target_file_path)) {
+                $move_success = true;
+                $result = mysqli_query($conn, "UPDATE competitors SET file_flag=1, file_url='$target_file_path' WHERE email='$email'");
+            }
+        }
+
+        // then
+        $this->assertTrue($move_success);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @depends testConnection
+     * @depends testLogin
+     */
+    public function testInsertPhoto(mysqli $conn, int $user_id): void
+    {
+        // this is the code in file-handler.php for file upload
+        // given initials
+        // imitation of $_FILES associative array
+        $file = array(
+            'name' => 'foto-ganteng.jpg',
+            'type' => 'image/jpeg',
+            'tmp_name' => '/tmp/phpn3FmFr',
+            'error' => 0,
+            'size' => 15476
+        );
+        $move_success = false;
+        $errors = array();
+        $result = false;
+        $user_root_dir = "/xampp/htdocs/ppl-app";
+
+        // do
+        // Check uploaded file type, reject if doesn't match
+        if (!preg_match("/^image\/(png|jp(e?)g)$/", $file['type'])) {
+            $errors[] = "File hanya dapat berupa png, jpg, atau jpeg.";
+        }
+
+        // Check if image file is an actual image or fake image
+        try {
+            !getimagesize($file['tmp_name']);
+        }
+        catch(Exception $e) {
+            $errors[] =  "File gambar tidak sesuai.";
+        }
+
+        if (count($errors) == 0) {
+            $target_file_path = "$user_root_dir/user-$user_id-photo.".strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+            if (move_uploaded_file($file['tmp_name'], $target_file_path)) {
+                $move_success = true;
+                $result = mysqli_query($conn, "UPDATE competitors SET photo_flag=1, photo_url='$target_file_path' WHERE email='$email'");
+            }
+        }
+
+        // then
+        $this->assertTrue($move_success);
+        $this->assertTrue($result);
+    }
+
+    /**
+     * @depends testConnection
+     * @depends testLogin
+     */
+    public function testInsertNoPhoto(mysqli $conn, int $user_id): void
+    {
+        // this is the code in file-handler.php for file upload
+        // given initials
+        // imitation of $_FILES associative array
+        $file = array(
+            'name' => '',
+            'type' => '',
+            'tmp_name' => '',
+            'error' => '',
+            'size' => ''
+        );
+        $move_success = false;
+        $errors = array();
+        $result = false;
+        $user_root_dir = "/xampp/htdocs/ppl-app";
+
+        // do
+        // Check uploaded file type, reject if doesn't match
+        if (!preg_match("/^image\/(png|jp(e?)g)$/", $file['type'])) {
+            $errors[] = "File hanya dapat berupa png, jpg, atau jpeg.";
+        }
+        // Check if image file is an actual image or fake image
+        if (
+            preg_match("/^image\/(png|jp(e?)g)$/", $file['type']) &&
+            !getimagesize($file['tmp_name'])
+        ) {
+            $errors[] =  "File gambar tidak sesuai.";
+        }
+
+        if (count($errors) == 0) {
+            $target_file_path = "$user_root_dir/user-$user_id-foto.".strtolower(pathinfo($file['name'], PATHINFO_EXTENSION));
+            if (move_uploaded_file($file['tmp_name'], $target_file_path)) {
+                $move_success = true;
+                $result = mysqli_query($conn, "UPDATE competitors SET photo_flag=1, photo_url='$target_file_path' WHERE email='$email'");
+            }
+        }
+
+        // then
+        $this->assertTrue($move_success);
+        $this->assertTrue($result);
     }
 
     /**
